8bf01d73830b3e9f34c98e851f02d86721a54583
BQ <30980148+bquirin@users.noreply.github.com>
2019-07-30 21:02:39 +0100

    Add files via upload

--- house_webscraper.py
+++ house_webscraper.py
@@ -13,21 +13,34 @@ def get_page(url):
 
 def get_data(soup):
     all_houses = []
-    divs = soup.findall("div", class_="test")
+    divs = soup.find_all("div", class_="l-searchResult is-list is-not-grid")
     for div in divs:
         all_houses.append({
-            "title": div.find("h1").text,
-            "price": div.find("h2").text
+            "description": div.find("h2", class_="propertyCard-title").text,
+            "price": div.find("div", class_="propertyCard-priceValue").text
 
         })
 
     return all_houses
 
 
+def write_csv(all_houses):
+    with open("rightmoveData.csv", "w") as file:
+        writer = csv.writer(file)
+        writer.writerows(["test", "testing2", "testing3"])
+    return "Done"
+
+
 def main():
-    url = "https://www.rightmove.co.uk/"
+    url = ("https://www.rightmove.co.uk/property-for-sale/find.html?searchType"
+           "=SALE&locationIdentifier=REGION%5E85310&insId=1&radius=0.0&min"
+           "Price=&maxPrice=&minBedrooms=&maxBedrooms=&displayPropertyType="
+           "&maxDaysSinceAdded=&_includeSSTC=on&sortByPriceDescending=&primary"
+           "DisplayPropertyType=&secondaryDisplayPropertyType=&oldDisplayProp"
+           "ertyType=&oldPrimaryDisplayPropertyType=&newHome=&auction=false")
     webpage = get_page(url)
-    print(get_data(webpage))
+    houses_list = get_data(webpage)
+    write_csv(houses_list)
 
 
 if __name__ == "__main__":
